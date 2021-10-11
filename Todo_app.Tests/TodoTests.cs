6110051c6abe545cd34bb0056d4299d6afddbfd7
madiha gul <mmadihagull@gmail.com>
2021-10-11 23:30:43 +0200

    Added TestProject in Solution

--- Todo_app.Tests/TodoTests.cs
+++ Todo_app.Tests/TodoTests.cs
@@ -1,14 +1,64 @@
 using System;
 using Xunit;
+using Todo_app.Model;
 
 namespace Todo_app.Tests
 {
     public class TodoTests
     {
-        [Fact]
-        public void Tests()
-        {
+      
+            [Theory]
+            [InlineData(1)]
+            [InlineData(-1)]
+            [InlineData(9999999)]
 
+            public void ConstructorTodorIdCheck(int todoId)            
+            {
+                string description = "test";
+
+                Todo testTodo = new Todo(todoId, description);
+
+                Assert.Equal(testTodo.TodoId, todoId);
+            }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("test")]
+
+        public void ConstructorTodoDescriptionCheck(string description)
+        {
+            int todoId = 1;
+
+            Todo testTodo = new Todo(todoId, description);
+
+            Assert.Equal(testTodo.Description, description);
         }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+
+        public void PropertyDoneCheck(bool done)
+        {
+            int todoId = 1;
+            string description = "test";
+            Todo testTodo = new Todo(todoId, description);
+            testTodo.Done = done;
+            Assert.Equal(testTodo.Done, done);
+        }
+        // Test class Person type object with proper assert
+        //[Theory]
+        //[InlineData()]
+        //[InlineData()]
+
+        //public void PropertyAssigneeCheck(bool assignee)
+        //{
+        //    int todoId = 1;
+        //    string description = "test";
+        //    Todo testTodo = new Todo(todoId, description);
+        //    testTodo.Assignee = assignee;
+        //    Assert.(testTodo.Assignee, assignee);
+        //}
     }
 }
--- Todo_app/Model/Todo.cs
+++ Todo_app/Model/Todo.cs
@@ -4,20 +4,26 @@ using System.Text;
 
 namespace Todo_app.Model
 {
-    class Todo
+    public class Todo
     {
+        // Created private readonly todoId, private description,done and assignee as per requirement in the assignment
         private readonly int todoId;
+        private string description;
+        private bool done;
+        private Person assignee;
+        // Property for private member todoId to access outside class
         public int TodoId { get { return todoId; } }
         
-        private string description;
+        // Property for private member description to access outside class
         public string Description { get { return description; } set{ description = value; } }
-
-        private bool done;
+        
+        // Property for private member done to access outside class
         public bool Done { get { return done; } set { done = value; } }
-
-        private Person assignee;
+        
+        // Property for private member assignee to access outside class
         public Person Assignee { get { return assignee; } set { assignee = value; } }
 
+        // Created Todo constructor as per requirement in the assignment
 
         public Todo(int todoId,string description )
         {
