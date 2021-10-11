b8aa3d5bf3802bf93da3727a7918fc520b8f9762
madiha gul <mmadihagull@gmail.com>
2021-10-11 22:39:22 +0200

    Test project added in the solution

---
+++ Todo_app.Tests/TodoTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Todo_app.Tests
+{
+    public class TodoTests
+    {
+        [Fact]
+        public void Tests()
+        {
+
+        }
+    }
+}
--- Todo_app/Model/Todo.cs
+++ Todo_app/Model/Todo.cs
@@ -6,7 +6,23 @@ namespace Todo_app.Model
 {
     class Todo
     {
-
+        private readonly int todoId;
+        public int TodoId { get { return todoId; } }
         
+        private string description;
+        public string Description { get { return description; } set{ description = value; } }
+
+        private bool done;
+        public bool Done { get { return done; } set { done = value; } }
+
+        private Person assignee;
+        public Person Assignee { get { return assignee; } set { assignee = value; } }
+
+
+        public Todo(int todoId,string description )
+        {
+            this.todoId = todoId;
+            this.description = description;
+        }
     }
 }
