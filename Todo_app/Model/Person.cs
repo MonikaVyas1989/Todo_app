7b0a9b955b702b2e7eaac7a2e8bf84fcc5147d42
MonikaVyas1989 <89772349+MonikaVyas1989@users.noreply.github.com>
2021-10-12 11:46:56 +0200

    Person Class Created

--- Todo_app/Model/Person.cs
+++ Todo_app/Model/Person.cs
@@ -4,8 +4,50 @@ using System.Text;
 
 namespace Todo_app.Model
 {
-    class Person
+    public class Person
     {
+        //Declaration of private fields of Class Person...
+        private readonly  int personId;
+        private string firstName, lastName;
 
+        public Person()
+        {
+           /* this.personId = 1;
+            this.firstName = "Monika";
+            this.lastName = "Vyas";*/
+        }
+
+        //Property to get value of PersonId ..
+        public int PersonId { get { return personId; } }
+
+        //Property to access of first name which can not be null or empty..
+        public string FirstName
+        {
+            get
+            { return firstName; }  
+            set 
+            {
+                if(value .Equals (""))
+                {
+                    throw new ArgumentNullException("First name must have a value..");
+                }
+                firstName = value;
+            }    
+        }
+
+        //Property to access of last name which can not be null or empty..
+        public string LastName
+        {
+            get {return lastName;} 
+            set
+            {
+                if (value.Equals(""))
+                {
+                    throw new ArgumentNullException("Last name must have a value..");
+                }
+                lastName = value; 
+            }
+        }
     }
+
 }
