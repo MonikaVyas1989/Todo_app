5e0624d22620496ba8f48b205902df38e7682259
MonikaVyas1989 <89772349+MonikaVyas1989@users.noreply.github.com>
2021-10-14 10:45:35 +0200

    Personsequencer added

---
+++ Todo_app.Tests/PersonSequencerTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Todo_app.Data;
+
+namespace Todo_app.Tests
+{
+    public class PersonSequencerTests
+    {
+        [Fact]
+
+        public void PersonSequencertest()
+        {
+            int id = 0;
+
+            Assert.Equal(PersonSequencer.PersonId, id);
+
+        }
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+
+        public void PersonIdIncreamentTest(int nextId)
+        {
+
+
+            int PersonnextId = PersonSequencer.NextPersonId();
+            Assert.Equal(nextId, PersonnextId);
+
+        }
+
+        [Fact]
+        public void PersonNextIdTest()
+        {
+            int nextIdTest = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                nextIdTest = i + 1;
+                int PersonnextId = PersonSequencer.NextPersonId();
+                Assert.Equal(nextIdTest, PersonnextId);
+            }
+
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(101)]
+        [InlineData(9)]
+        public void PersonIdResetTests(int id)
+        {
+
+            int resetPersonId = PersonSequencer.Reset();
+
+            Assert.Equal(0, resetPersonId);
+        }
+
+    }
+}
+
---
+++ Todo_app/Data/PersonSequencer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo_app.Data
+{
+    public class PersonSequencer
+    {
+        //Creation of private field of Class PersonSequencer..
+        private static int personId;
+
+        //Property to get value of PersonId ..
+        public static int PersonId { get { return personId; } }
+
+
+        //Creation of Method to give increement in PersonId..
+        public static int NextPersonId()
+        {
+
+            return ++personId;
+
+        }
+
+        //Creation of Method to make PersonId value 0..
+        public static int Reset()
+        {
+
+            return personId = 0;
+        }
+    }
+}
+
